a4ae91eaf12489d340236b3e231f6f65f0f167d7
Gnm <gmang2236@gmail.com>
2022-07-16 01:23:46 +0300

    Set header title and desctiption

--- components/Layout/AppLayout.tsx
+++ components/Layout/AppLayout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { ReactElement, useState } from 'react';
 
 import Search from '../Search/Search';
@@ -10,12 +11,21 @@ const AppLayout = ({
   const [previousPage, setPreviousPage] = useState('');
 
   return (
-    <div className='bg-CustomBlack min-h-screen relative'>
-      <div className='container mx-auto pt-6 px-3 sm:px-0 relative'>
-        <Search previousPage={previousPage} />
-        <main>{children}</main>
+    <>
+      <Head>
+        <title>Onliradio</title>
+        <meta
+          name='description'
+          content='Listen to all your favorite radio stations on the go'
+        />
+      </Head>
+      <div className='bg-CustomBlack min-h-screen relative'>
+        <div className='container mx-auto pt-6 px-3 sm:px-0 relative'>
+          <Search previousPage={previousPage} />
+          <main>{children}</main>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default AppLayout;
