358e2cdd1a1f538b985b06dd7ef0e93998621945
Gnm <gmang2236@gmail.com>
2022-07-16 00:49:45 +0300

    Utilities to respond with random server url

---
+++ util/getUrl.ts
@@ -0,0 +1,23 @@
+import { lookup, reverse } from 'dns/promises';
+
+export async function getRadioServerUrl(): Promise<string> {
+  const ipLookup = await lookup('all.api.radio-browser.info', {
+    all: true,
+    family: 4,
+  }).catch((error) => {
+    console.log(error);
+    throw new Error('Error looking up Dns');
+  });
+  const urlsfromip = ipLookup.map(async ({ address }) => {
+    try {
+      const result = await reverse(address);
+      return result.toString();
+    } catch (error) {
+      throw Error('Dns Reverse failed');
+    }
+  });
+  const response = await Promise.all(urlsfromip);
+  console.log(response);
+  const requestAddress = Math.floor(Math.random() * response.length);
+  return `https://${response[requestAddress]}`;
+}
