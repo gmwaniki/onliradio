5002ac8cf5a87dd1346c56cef7d1cff6af9feb1d
Gnm <gmang2236@gmail.com>
2022-07-16 00:44:14 +0300

    added custom: - font family - colors - background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        CustomPurple: '#D1D9FF',
+        CustomActivePurple: '#BA78FC',
+        CustomVeryPurple: '#482284',
+        CustomLightPurple: '#B6C3FF',
+
+        CustomWhite: '#F2F4FF',
+        CustomBlack: '#040404',
+        // Custom
+        CustomBackgroundBlack: '#161616',
+        CustomLightGrey: '#6C6C6C',
+        CustomTextGrey: '#9A9A9A',
+      },
+      backgroundImage: {
+        gradient: 'linear-gradient(to right, #4B1E7A, #2E47E6)',
+      },
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
