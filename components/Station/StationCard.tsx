96ec31795b684fd21fb9e0cb507fb4b624e474a2
Gnm <gmang2236@gmail.com>
2022-07-17 21:00:51 +0300

    Converted card to Component

---
+++ components/Station/StationCard.tsx
@@ -0,0 +1,67 @@
+import Link from 'next/link';
+import { ReactElement } from 'react';
+import { HiExternalLink, HiPlay } from 'react-icons/hi';
+import { MdTranslate } from 'react-icons/md';
+import { TStation } from '../../util/playableStation';
+
+const StationCard = ({
+  station,
+  className,
+}: {
+  station: TStation;
+  className?: string;
+}): JSX.Element => {
+  const { homepage, name, favicon, language } = station;
+
+  return (
+    <section
+      className={`bg-CustomBackgroundBlack grid grid-flow-col mt-4 pb-4 rounded-md ${className}`}
+    >
+      <div className='grid grid-cols-3 grid-rows-[auto_1fr_auto] px-2 '>
+        <div className='col-start-1 col-end-4 flex justify-between items-center'>
+          <Link href={homepage}>
+            <a target='_blank'>
+              <span className='sr-only'>Go to radio station website</span>
+              <HiExternalLink
+                className='text-3xl hover:fill-CustomActivePurple'
+                aria-hidden='true'
+              />
+            </a>
+          </Link>
+
+          <button>
+            <span className='sr-only'>Play station</span>
+            <HiPlay className='text-6xl mt-1 hover:fill-CustomActivePurple  bg-clip-border' />
+          </button>
+        </div>
+        <div className='col-span-3 flex justify-center items-center'>
+          <picture className='relative w-[35%]  flex justify-center items-center'>
+            <img
+              src={favicon || '/images/logo/cover.png'}
+              alt='User image'
+              width='300px'
+              height='300px'
+              className='object-contain'
+            />
+          </picture>
+        </div>
+        <div className='mt-4'>
+          <h2
+            className='text-ellipsis overflow-hidden whitespace-nowrap block'
+            title={name}
+          >
+            {name}
+          </h2>
+          <div className='flex items-center'>
+            <div className='sr-only'>Language</div>
+            <MdTranslate aria-hidden='true' />
+            <span className='ml-1 capitalize'>{language}</span>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default StationCard;
