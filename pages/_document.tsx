43e154f1db42c02f14e006a55b285bab8c12c276
Gnm <gmang2236@gmail.com>
2022-07-20 00:22:31 +0300

    set document charset

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap'
             rel='stylesheet'
           />
+          <meta charSet='UTF-8' />
         </Head>
         <body>
           <Main />
