f05d9a4092b3ed3e27f884bf3718800d9daa1454
Gnm <gmang2236@gmail.com>
2022-07-20 00:47:51 +0300

    Added station context and state

--- components/Station/StationCard.tsx
+++ components/Station/StationCard.tsx
@@ -1,19 +1,33 @@
 import Link from 'next/link';
-import { ReactElement } from 'react';
-import { HiExternalLink, HiPlay } from 'react-icons/hi';
+import React, { ReactElement, useContext } from 'react';
+import {
+  HiExternalLink,
+  HiMusicNote,
+  HiOutlineMusicNote,
+  HiOutlinePlay,
+  HiPlay,
+} from 'react-icons/hi';
 import { MdTranslate } from 'react-icons/md';
+import {
+  stationContext,
+  StationReducerActionType,
+} from '../../Context/AudioContext';
+import getFlagEmoji from '../../util/getFlagEmoji';
 import { TStation } from '../../util/playableStation';
 
 const StationCard = ({
   station,
   className,
   refCallback,
+  isPlaying,
 }: {
   station: TStation;
   className?: string;
+  isPlaying?: boolean;
   refCallback: (el: HTMLImageElement) => void;
 }): JSX.Element => {
-  const { homepage, name, favicon, language } = station;
+  const { homepage, name, favicon, language, countrycode, country } = station;
+  const { dispatch } = useContext(stationContext);
 
   return (
     <section
@@ -31,30 +45,42 @@ const StationCard = ({
             </a>
           </Link>
 
-          <button>
+          <button
+            onClick={() => {
+              dispatch({
+                type: StationReducerActionType.PLAY,
+                payload: station,
+              });
+            }}
+          >
             <span className='sr-only'>Play station</span>
-            <HiPlay className='text-6xl mt-1 hover:fill-CustomActivePurple  bg-clip-border' />
+
+            <HiOutlinePlay className='[&_path]:stroke-1  text-6xl mt-1 hover:stroke-CustomActivePurple ' />
           </button>
         </div>
-        <div className='col-span-3 flex justify-center items-center'>
-          <picture className='relative w-[35%]  flex justify-center items-center'>
-            <img
-              src='/images/logo/profile.png'
-              data-src={favicon || '/images/logo/profile.png'}
-              alt={name}
-              width='300px'
-              height='300px'
-              className='object-contain '
-              ref={refCallback}
-            />
-          </picture>
+        <div className='col-span-3 flex justify-center items-center relative w-[35%] mx-auto'>
+          {favicon ? (
+            <picture>
+              <img
+                src='/images/musicnote.svg'
+                data-src={favicon}
+                alt={name}
+                width='300px'
+                height='300px'
+                className='object-contain rounded'
+                ref={refCallback}
+              />
+            </picture>
+          ) : (
+            <HiOutlineMusicNote className='w-full h-full svgthin' />
+          )}
         </div>
-        <div className='mt-4'>
+        <div className='mt-4 col-span-2'>
           <h2
-            className='text-ellipsis overflow-hidden whitespace-nowrap block'
+            className='text-ellipsis overflow-hidden whitespace-nowrap block capitalize'
             title={name}
           >
-            {name}
+            {name.toUpperCase()}
           </h2>
           <div className='flex items-center'>
             <div className='sr-only'>Language</div>
@@ -62,10 +88,15 @@ const StationCard = ({
             <span className='ml-1 capitalize'>{language}</span>
           </div>
         </div>
-        <div></div>
+        <div
+          className='text-2xl col-start-3 justify-self-center self-center'
+          title={country}
+        >
+          {getFlagEmoji(countrycode)}
+        </div>
       </div>
     </section>
   );
 };
 
-export default StationCard;
+export default React.memo(StationCard);
