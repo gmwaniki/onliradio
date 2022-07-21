82e0f130058da3992a586a4303647c42b10b4834
Gnm <gmang2236@gmail.com>
2022-07-21 14:02:07 +0300

    set up header and index page tests

---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../../src/pages/index';
+import '@testing-library/jest-dom';
+
+describe('HomePage', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  it('Should check if homepage loads without any errors', () => {
+    render(<Home />);
+  });
+  it('Should Check if link to app works', async () => {
+    render(<Home />);
+    const link = screen.getByRole('link', { name: /Listen$/i });
+    expect(link).toHaveAttribute('href', '/app');
+
+    // await waitFor(() => screen.getByPlaceholderText(/enter a station name/i));
+  });
+});
