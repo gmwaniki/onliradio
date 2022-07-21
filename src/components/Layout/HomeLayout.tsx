058681fec5a63aabb5069f900e9d4e3a5bf810de
Gnm <gmang2236@gmail.com>
2022-07-22 01:02:23 +0300

    set title and description on homepage

--- src/components/Layout/HomeLayout.tsx
+++ src/components/Layout/HomeLayout.tsx
@@ -1,14 +1,24 @@
+import Head from 'next/head';
 import { ReactNode } from 'react';
 import Header from '../Header/Header';
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='min-h-screen bg-CustomWhite relative'>
-      <div className='container mx-auto pt-5 px-6 sm:px-0 md:px-6 md:pt-7 grid grid-flow-row gap-10 '>
-        <Header />
-        <main className='sm:px-6 justify-self-end'>{children}</main>
+    <>
+      <Head>
+        <title>Onliradio</title>
+        <meta
+          name='description'
+          content='Listen to over 30,000 radio station from over 200 countries '
+        />
+      </Head>
+      <div className='min-h-screen bg-CustomWhite relative'>
+        <div className='container mx-auto pt-5 px-6 sm:px-0 md:px-6 md:pt-7 grid grid-flow-row gap-10 '>
+          <Header />
+          <main className='sm:px-6 justify-self-end'>{children}</main>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default HomeLayout;
