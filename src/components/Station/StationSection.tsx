638aa51f582a6f40fb1b39177151dd7be80f4112
Gnm <gmang2236@gmail.com>
2022-08-09 18:13:07 +0300

    make component reusable by using props

--- src/components/Station/StationSection.tsx
+++ src/components/Station/StationSection.tsx
@@ -1,24 +1,16 @@
-import { useCallback, useRef } from 'react';
 import { useStationState } from '../../Context/AudioContext';
-import useInterSectionObserver from '../../hooks/useIntersectionObserver';
 import { TStation } from '../../util/playableStation';
 import StationCard from './StationCard';
 
 const StationSection = ({
   stations,
+  title,
 }: {
   stations: TStation[];
   title: string;
 }) => {
   const { state } = useStationState();
-  const ref = useRef<HTMLImageElement[]>([]);
-  useInterSectionObserver(ref);
-  const setImageElementRef = useCallback((el: HTMLImageElement) => {
-    if (ref.current) {
-      if (!el) return;
-      ref.current.push(el);
-    }
-  }, []);
+
   const isplaying = (station: TStation) => {
     if (state === null || state.isPlaying === undefined) return null;
     if (state.stationuuid !== station.stationuuid) {
@@ -30,7 +22,7 @@ const StationSection = ({
   return (
     <section className='col-start-1 col-span-full overflow-x-auto'>
       <div className=''>
-        <h1 className='font-semibold text-lg break-words'>Stations by Votes</h1>
+        <h1 className='font-semibold text-lg break-words'>{title}</h1>
       </div>
       <div className=' flex overflow-x-scroll scroll-smooth snap-x snap-mandatory [&>*+*]:ml-[.85rem] scrollbar sm:px-2 pt-2 md:snap-none '>
         {stations.map((station) => {
@@ -38,7 +30,6 @@ const StationSection = ({
             <StationCard
               station={station}
               key={station.stationuuid}
-              refCallback={setImageElementRef}
               isPlaying={isplaying(station)}
             />
           );
