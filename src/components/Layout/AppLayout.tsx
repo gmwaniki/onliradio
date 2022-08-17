9ca9e4b39a25b414da78e05c1aea7f7aac2c52b5
Gnm <gmang2236@gmail.com>
2022-08-17 21:20:04 +0300

    Add Providers - React Query Provider - Search Provider

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -1,36 +1,43 @@
 import Head from 'next/head';
-import { ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
 import StationContextProvider from '../../Context/AudioContext';
+import SearchContextProvider from '../../Context/SearchContext';
 import Player from '../Player/Player';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Search from '../Search/Search';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const AppLayout = ({
   children,
 }: {
   children: ReactElement | ReactElement[];
 }) => {
-  const [previousPage, setPreviousPage] = useState('');
-
+  const queryClient = new QueryClient();
   return (
     <>
-      <Head>
-        <title>Onliradio</title>
-        <meta
-          name='description'
-          content='Listen to all your favorite radio stations on the go'
-        />
-        <link rel='icon' href='/images/logo/favicon/icons-96.png' />
-      </Head>
-      <div className='bg-CustomBlack relative h-screen  overflow-x-hidden'>
-        <div className=' mx-auto pt-6 px-3 sm:px-3   '>
-          <Search previousPage={previousPage} />
-          <StationContextProvider>
-            <main className='container mx-auto pb-32'>{children}</main>
-            <Player />
-          </StationContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <Head>
+          <title>Onliradio</title>
+          <meta
+            name='description'
+            content='Listen to all your favorite radio stations on the go'
+          />
+          <link rel='icon' href='/images/logo/favicon/icons-96.png' />
+        </Head>
+        <div className='bg-CustomBlack relative h-screen  overflow-x-hidden'>
+          <div className=' mx-auto pt-6 px-3 sm:px-3   '>
+            <SearchContextProvider>
+              <Search />
+              <StationContextProvider>
+                <main className='xl:container mx-auto pb-32'>{children}</main>
+                <Player />
+              </StationContextProvider>
+            </SearchContextProvider>
+          </div>
         </div>
-      </div>
+      </QueryClientProvider>
     </>
   );
 };
