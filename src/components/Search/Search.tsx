5cf872d08fcc1378a9cd973ac1dbc481f8fd0f74
Gnm <gmang2236@gmail.com>
2022-08-17 21:24:04 +0300

    Setup filters an global context

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,96 +1,162 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import { HiChevronDown } from 'react-icons/hi';
-import { MdArrowBack, MdSearch } from 'react-icons/md';
+import React, { useContext, useState } from 'react';
+import { HiHome } from 'react-icons/hi';
+import { MdSearch } from 'react-icons/md';
+import {
+  SearchContext,
+  TCheckBoxes,
+  TInputValues,
+  TSearchReducerEnum,
+} from '../../Context/SearchContext';
+import SearchTextInput from './SearchTextInput';
 
-const Search = ({
-  previousPage,
-  className,
-}: {
-  previousPage: string;
-  className?: string;
-}) => {
-  const [selectValue, setSelectValue] = useState('');
-  const [searchInput, setSearchInput] = useState<string>('');
+const Search = ({}: {}) => {
+  const { dispatch, state } = useContext(SearchContext);
+  const [inputValues, setInputValues] = useState<TInputValues>({
+    country: '',
+    genre: '',
+    language: '',
+    name: '',
+  });
+  const [inputCheckboxes, setInputCheckboxes] = useState<TCheckBoxes>({
+    country: false,
+    genre: false,
+    language: false,
+    name: true,
+  });
+
   const router = useRouter();
-  function handleBackClick(e: React.SyntheticEvent<HTMLButtonElement>) {
-    if (!previousPage) {
-      return router.replace('/app');
-    }
-    router.replace(previousPage);
-    return;
+
+  const activeCheckboxes = Object.entries(inputCheckboxes)
+    .reverse()
+    .filter((item) => {
+      return item[1];
+    });
+  function handleCheckbox(e: React.SyntheticEvent<HTMLInputElement>) {
+    const checkboxName: string = e.currentTarget.name;
+    setInputCheckboxes({
+      ...inputCheckboxes,
+      [checkboxName]: !inputCheckboxes[checkboxName],
+    });
   }
-  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearchInput(e.currentTarget.value);
+  function handleFormSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const isSearchPage = /search/i.test(router.asPath);
+    if (!isSearchPage) {
+      router.push('/app/search');
+    }
+    dispatch({
+      type: TSearchReducerEnum.SEARCH,
+      payload: {
+        filter: inputCheckboxes,
+        searchValues: inputValues,
+      },
+    });
   }
 
   return (
-    <div className='sticky top-4 z-10'>
+    <div className='sm:sticky sm:top-2 sm:z-10 max-w-[1920px] mx-auto'>
       <form
-        onFocus={() => router.prefetch('/app/seart')}
-        onSubmit={(e) => {
-          e.preventDefault();
-          router.push('/app/search');
-        }}
+        className='bg-[#3D3D3D]/60 backdrop-blur  grid grid-flow-row px-3 py-2  rounded gap-4 sm:grid-cols-[auto,minmax(0,1fr),130px] sm:items-end'
+        onFocus={() => router.prefetch('/app/search')}
+        onSubmit={handleFormSubmit}
       >
-        <div className='grid grid-cols-[1fr_auto] px-3 py-2 bg-CustomBackgroundBlack rounded'>
-          <label className='relative border-r-2 border-r-CustomBackgroundBlack'>
-            <span className='sr-only'>
-              Search for a radio station by entering a {selectValue || 'name'}
-            </span>
+        <Link href={'/app'}>
+          <a
+            className='bg-CustomBlack text-CustomWhite p-4 rounded-md grid grid-flow-col gap-2 items-start w-min justify-start hover:bg-CustomBlack/70 focus:bg-CustomBlack/70 focus:ring-CustomActivePurple focus:ring-4 focus:outline-none'
+            onClick={() => {
+              router.replace('/app');
+            }}
+          >
+            <HiHome className='fill-CustomWhite text-2xl' />
+            <span>Home</span>
+          </a>
+        </Link>
+        <div className='grid grid-flow-row gap-y-2 sm:grid-flow-col sm:gap-x-2'>
+          {activeCheckboxes.length <= 0 ? (
+            <SearchTextInput
+              name='name'
+              key='name'
+              value={inputValues}
+              setValue={setInputValues}
+            />
+          ) : (
+            activeCheckboxes.map((checkedCheckbox) => {
+              return (
+                <SearchTextInput
+                  name={checkedCheckbox[0]}
+                  key={checkedCheckbox[0]}
+                  value={inputValues}
+                  setValue={setInputValues}
+                />
+              );
+            })
+          )}
+        </div>
+
+        <div className='text-CustomWhite grid grid-cols-2  sm:grid-cols-[.2fr,repeat(4,minmax(0,1fr))] gap-2 sm:justify-center items-center sm:col-span-2 sm:col-start-2'>
+          <div className='col-span-2 sm:col-auto justify-self-start mr-2'>
+            Filters:
+          </div>
+          <label className='flex items-center'>
             <input
-              type='text'
-              className='h-full w-full rounded-l-md py-4 pr-4 pl-10 peer text-CustomWhite bg-CustomLightGrey  placeholder:text-CustomWhite outline-none focus:ring-1 ring-CustomActivePurple'
-              placeholder={`Enter a ${selectValue || 'station name'}`}
-              value={searchInput}
-              onChange={handleInputChange}
-            />
-
-            <MdSearch
-              className='absolute top-1/2 -translate-y-1/2 ml-2 text-3xl opacity-0 transition-[opacity] duration-300 peer-placeholder-shown:fill-CustomWhite peer-placeholder-shown:opacity-100'
-              aria-hidden='true'
-            />
-
-            <button
-              className='absolute top-1/2 -translate-y-1/2  left-0 h-full opacity-100 delay-300  peer-placeholder-shown:opacity-0 peer-placeholder-shown:delay-75'
-              type='button'
-              onClick={handleBackClick}
-              disabled={!searchInput}
-            >
-              <span className='sr-only'>Go back</span>
-              <MdArrowBack
-                className='ml-2 text-3xl fill-CustomWhite'
-                aria-hidden='true'
-              />
-            </button>
-          </label>
-          <label className='relative group'>
-            <span className='absolute h-full w-full bg-CustomLightGrey text-CustomWhite  pointer-events-none grid grid-cols-[2fr_1fr] justify-center items-center rounded-r-md group-focus-within:ring-1 ring-CustomActivePurple'>
-              <span className='justify-self-center capitalize truncate w-[6ch] inline-block '>
-                {selectValue}
-              </span>
-              <HiChevronDown className='fill-CustomWhite text-2xl justify-self-center' />
-            </span>
-            <select
-              name='searchCategory'
+              type='checkbox'
+              name='name'
               id=''
-              className='h-[95%] my-[2%]  bg-CustomWhite outline-none w-[95%] mx-[2%]'
-              onChange={(e) => {
-                setSelectValue(e.target.value);
-              }}
-              title={selectValue}
-            >
-              <option value=''></option>
-              <option value='name'>Name</option>
-              <option value='country'>Country</option>
-              <option value='language'>Language</option>
-              <option value='genre'>Genre</option>
-            </select>
+              className='appearance-none  w-6 h-6 rounded relative 
+              after:w-full after:h-full after:flex after:justify-center after:items-center after:absolute 
+              after:rounded after:bg-CustomTextGrey checked:after:content-["✓"] checked:after:bg-CustomActivePurple '
+              checked={inputCheckboxes.name}
+              onChange={handleCheckbox}
+            />
+            <div className='ml-2'>Station name</div>
+          </label>
+          <label className='flex items-center'>
+            <input
+              type='checkbox'
+              name='language'
+              id=''
+              className='appearance-none  w-6 h-6 rounded relative 
+              after:w-full after:h-full after:flex after:justify-center after:items-center after:absolute after:rounded after:bg-CustomTextGrey checked:after:content-["✓"] checked:after:bg-CustomActivePurple '
+              checked={inputCheckboxes.language}
+              onChange={handleCheckbox}
+            />
+            <div className='ml-2'>Language</div>
+          </label>
+
+          <label className='flex items-center'>
+            <input
+              type='checkbox'
+              name='genre'
+              id=''
+              className='appearance-none  w-6 h-6 rounded relative 
+              after:w-full after:h-full after:flex after:justify-center after:items-center after:absolute after:rounded after:bg-CustomTextGrey checked:after:content-["✓"] checked:after:bg-CustomActivePurple '
+              checked={inputCheckboxes.genre}
+              onChange={handleCheckbox}
+            />
+            <div className='ml-2'>Genre</div>
+          </label>
+          <label className='flex items-center'>
+            <input
+              type='checkbox'
+              name='country'
+              id=''
+              className='appearance-none  w-6 h-6 rounded relative 
+              after:w-full after:h-full after:flex after:justify-center after:items-center after:absolute after:rounded after:bg-CustomTextGrey checked:after:content-["✓"] checked:after:bg-CustomActivePurple '
+              checked={inputCheckboxes.country}
+              onChange={handleCheckbox}
+            />
+            <div className='ml-2'>Country</div>
           </label>
         </div>
+        <button className='bg-CustomActivePurple text-CustomWhite py-[14px] rounded-md text-xl flex justify-center items-center sm:col-start-3 sm:row-start-1 '>
+          <MdSearch className='text-2xl' />
+          <span className='ml-1'>Search</span>
+        </button>
       </form>
     </div>
   );
 };
 
-export default React.memo(Search);
+export default Search;
