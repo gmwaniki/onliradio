a66f9ca0c550fa4911874e61e32a54b674f29209
Gnm <gmang2236@gmail.com>
2022-08-18 15:14:20 +0300

    setup webpack analyze

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: ['localhost'] },
 };
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+// module.exports = ;
+
+(module.exports = withPlugins([withBundleAnalyzer({})])), nextConfig;
