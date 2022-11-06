cb2c7fd3f196a633ae6b66f0217694d095a906d2
Gnm <gmang2236@gmail.com>
2022-11-06 19:36:06 +0300

    refactore: change to new link tag format
    This conforms to new next js link component syntax

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,17 +12,15 @@ const Header = () => {
     <>
       <header className=' grid grid-cols-2 sm:text-lg sm:grid-cols-[.5fr_2.5fr]  '>
         <Link href='/'>
-          <a>
-            <span className='sr-only'>Homepage</span>
-            <Image
-              src='/images/logo/vector/default-monochrome.svg'
-              alt='onliradio logo'
-              height='47px'
-              width='256px'
-              layout='responsive'
-              priority
-            />
-          </a>
+          <span className='sr-only'>Homepage</span>
+          <Image
+            src='/images/logo/vector/default-monochrome.svg'
+            alt='onliradio logo'
+            height='47'
+            width='256'
+            layout='responsive'
+            priority
+          />
         </Link>
         <div className='justify-self-end grid place-items-center sm:hidden'>
           <button onClick={handleMenuClick} className='text-3xl '>
@@ -37,40 +35,45 @@ const Header = () => {
         <nav className='hidden sm:grid sm:grid-cols-[1fr_auto]'>
           <ul className='grid grid-flow-col items-center justify-center gap-8'>
             <li>
-              <Link href='/'>
-                <a className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'>
-                  Home
-                </a>
+              <Link
+                href='/'
+                className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'
+              >
+                Home
               </Link>
             </li>
             <li>
-              <Link href='/'>
-                <a className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'>
-                  Contact us
-                </a>
+              <Link
+                href='/'
+                className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'
+              >
+                Contact us
               </Link>
             </li>
             <li>
-              <Link href='/'>
-                <a className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'>
-                  Faq
-                </a>
+              <Link
+                href='/'
+                className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'
+              >
+                Faq
               </Link>
             </li>
           </ul>
           <ul className='grid grid-flow-col items-center gap-8'>
             <li>
-              <Link href='/app'>
-                <a className='bg-gradient text-white py-[10px] px-[30px] rounded-md transition-transform duration-300 hover:opacity-90 '>
-                  Listen
-                </a>
+              <Link
+                href='/app'
+                className='bg-gradient text-white py-[10px] px-[30px] rounded-md transition-transform duration-300 hover:opacity-90 '
+              >
+                Listen
               </Link>
             </li>
             <li>
-              <Link href='/w'>
-                <a className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'>
-                  Sign up
-                </a>
+              <Link
+                href='/w'
+                className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'
+              >
+                Sign up
               </Link>
             </li>
           </ul>
