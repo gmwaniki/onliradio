6d2d47166a9d79ff474d62e4d4ddd8119d2c2b9f
Gnm <gmang2236@gmail.com>
2022-11-06 20:10:20 +0300

    refactor: remove bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require('next-compose-plugins');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: ['localhost'], unoptimized: true },
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-// module.exports = ;
-
-(module.exports = withPlugins([withBundleAnalyzer({})])), nextConfig;
+module.exports = nextConfig;
