78f3beba0f55370a1378e8bff3e18aedd18c8a1b
Gnm <gmang2236@gmail.com>
2022-11-06 20:28:03 +0300

    feat: renders previous stations

--- src/components/Station/StationsPlayed.tsx
+++ src/components/Station/StationsPlayed.tsx
@@ -1,13 +1,97 @@
-import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Image from 'next/image';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import {
+  HiOutlineMusicNote,
+  HiOutlinePlay,
+  HiPause,
+  HiPlay,
+} from 'react-icons/hi';
+import { StationContext } from '../../Context/AudioContext';
 import { TStation } from '../../util/playableStation';
+import { shimmer, toBase64 } from '../../util/shimmer';
 
-const StationsPlayed = () => {
-  const [stationsPlayed, setStationsPlayed] = useState<TStation[] | null>(null);
+type TProps = {
+  url: string;
+};
+
+type TRecentStation = {
+  station: TStation;
+  isPlaying: boolean;
+};
+const RecentStation = ({ station, isPlaying }: TRecentStation) => {
+  const [src, setSrc] = useState(`/api/image?url=${station.favicon}`);
+
+  return (
+    <div className='grid grid-cols-[.3fr,1fr,auto] gap-2 items-center'>
+      <span>
+        {station.favicon ? (
+          <Image
+            src={src}
+            alt={station.name}
+            width='100'
+            height='100'
+            placeholder='blur'
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              shimmer(100, 100)
+            )}`}
+            onError={() => setSrc('/images/musicnote.svg')}
+            className='rounded-md'
+          />
+        ) : (
+          <HiOutlineMusicNote className='w-full h-full svgthin' />
+        )}
+      </span>
+      <div className='text-left'>
+        <p className=''>{station.name}</p>
+        <p>{station.votes}</p>
+      </div>
+      <button>
+        {isPlaying === false ? (
+          <HiOutlinePlay className='svgthin  text-6xl mt-1 hover:stroke-CustomActivePurple ' />
+        ) : (
+          <HiPause className=' text-6xl mt-1 fill-CustomActivePurple ' />
+        )}
+      </button>
+    </div>
+  );
+};
+
+const StationsPlayed = ({ url }: TProps) => {
+  const [stations, setStations] = useState<TStation[]>([]);
+  const { storedStations } = useContext(StationContext);
+  const { state } = useContext(StationContext);
+  const { isPlaying, station: currentStation } = state;
 
   useEffect(() => {
-    console.log(localStorage.getItem('recentlyPlayedStations'));
-  }, []);
-  return <div>StationsPlayed</div>;
+    const getStations = async () => {
+      const params = new URLSearchParams({
+        uuids: storedStations.join(','),
+      });
+      const { href: stationUrl } = new URL(
+        `${url}/stations/byuuid?${params.toString()}`
+      );
+      const { data } = await axios.get(stationUrl);
+      setStations(data as TStation[]);
+    };
+    getStations();
+  }, [storedStations, url]);
+
+  return (
+    <div className=''>
+      {stations.map((station) => {
+        return (
+          <RecentStation
+            station={station}
+            key={station.stationuuid}
+            isPlaying={
+              station.stationuuid === currentStation.stationId && isPlaying
+            }
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default StationsPlayed;
