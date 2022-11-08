4837e2a76136723f5a3574e2c90e2e2c8c1ae119
Gnm <gmang2236@gmail.com>
2022-11-08 11:04:11 +0300

    feat: homepage head component  - migration to next 13  - website meta data an title

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Onliradio</title>
+      <meta
+        name='description'
+        content='Listen to over 30,000 radio station from over 200 countries '
+      />
+      <link rel='icon' href='/images/logo/favicon/icons-96.png' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+    </>
+  );
+}
