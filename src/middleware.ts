d19164e4ad3ecb1bbd4c6701432e048f03a25f50
Gnm <gmang2236@gmail.com>
2022-11-12 08:22:47 +0300

    refactor: converted middleware to use headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,16 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const { nextUrl, geo } = request;
+  const { geo, headers } = request;
   // const country = geo?.count
-  if (nextUrl.searchParams.has('country')) {
-    return NextResponse.next();
-  } else {
-    nextUrl.searchParams.set('country', `${geo?.country || 'KE'}`);
-
-    return NextResponse.redirect(nextUrl);
-  }
+  const requestHeaders = new Headers(headers);
+  requestHeaders.set('x-code', geo?.country || 'KE');
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  return response;
 }
 
 // See "Matching Paths" below to learn more
