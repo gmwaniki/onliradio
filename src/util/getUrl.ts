1c42a67307403ef61a80b5896e0e21ad0894c0a2
Gnm <gmang2236@gmail.com>
2022-11-16 12:43:26 +0300

    refactor: now using resolve srv

--- src/util/getUrl.ts
+++ src/util/getUrl.ts
@@ -1,22 +1,6 @@
-import { lookup, reverse } from 'dns/promises';
+import { resolveSrv } from 'dns/promises';
 
-export async function getRadioServerUrl(): Promise<string> {
-  const ipLookup = await lookup('all.api.radio-browser.info', {
-    all: true,
-    family: 4,
-  }).catch((error) => {
-    console.log(error);
-    throw new Error('Error looking up Dns');
-  });
-  const urlsfromip = ipLookup.map(async ({ address }) => {
-    try {
-      const result = await reverse(address);
-      return result.toString();
-    } catch (error) {
-      throw Error('Dns Reverse failed');
-    }
-  });
-  const response = await Promise.all(urlsfromip);
-  const requestAddress = Math.floor(Math.random() * response.length);
-  return `https://${response[requestAddress]}/json`;
-}
+export const getUrl = async () => {
+  const results = await resolveSrv('_api._tcp.radio-browser.info');
+  return `https://${results[0].name}/json`;
+};
