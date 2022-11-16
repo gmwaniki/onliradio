a135e8c5daf5523230cc6644be3271f0dc87444e
Gnm <gmang2236@gmail.com>
2022-11-16 12:38:46 +0300

    refactor:favourites page

---
+++ src/app/app/favourites/page.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+import FavouritesPage from '../../../components/Favourites/FavouritesPage';
+import { getUrl } from '../../../util/getUrl';
+
+export default async function Page() {
+  const url = await getUrl();
+
+  return <FavouritesPage url={url} />;
+}
