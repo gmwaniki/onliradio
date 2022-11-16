2aa6cdf82278bd3dbdf8c0db0906770f8b5c7c6a
Gnm <gmang2236@gmail.com>
2022-11-16 12:42:06 +0300

    refactor: player component

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,234 +1,126 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import {
-  HiOutlineHeart,
-  HiOutlineMusicNote,
-  HiPause,
-  HiPlay,
-  HiVolumeUp,
-} from 'react-icons/hi';
-
-import {
-  StationContext,
-  StationReducerActionType,
-  TActiveStation,
-  TStationAction,
-} from '../../Context/AudioContext';
-import useGetStation from '../../hooks/useGetStation';
-
+'use client';
+import Image from 'next/image';
+import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { HiHeart, HiOutlineHeart, HiPause, HiPlay } from 'react-icons/hi';
+import { AudioContext, StationReducerActionType } from '../../app/AudioContext';
 import getFlagEmoji from '../../util/getFlagEmoji';
-import { TStation, TstationApp } from '../../util/playableStation';
-import Spinner from '../Spinner/Spinner';
 
-type TPlayerProps = {
-  state: TActiveStation;
-  isPlaying: boolean;
-};
-const Player = ({ state, isPlaying }: TPlayerProps) => {
+export default function Player() {
+  const { state, dispatch } = useContext(AudioContext);
+  const { station, isPlaying } = state;
   const audioRef = useRef<HTMLAudioElement>(null);
-  const [audioStatus, setAudioStatus] = useState({ status: '', message: '' });
-  const { dispatch, dispatchStoreStation } = useContext(StationContext);
+  const [like, setLike] = useState<boolean>(false);
 
   useEffect(() => {
-    const audio = audioRef.current;
-    console.log('Loading audio');
-    if (audio && isPlaying) {
-      audio.load();
-      audio.play().catch((error) => {
-        //TODO handle error
-      });
-    }
-    return () => {
-      if (audio) {
-        audio.pause();
+    const audioElement = audioRef.current;
+
+    if (audioElement) {
+      if (isPlaying) {
+        audioElement.load();
+        audioElement.play().catch((error) => {
+          audioElement.pause();
+        });
+      } else {
+        audioElement.pause();
       }
-    };
-  }, [isPlaying, state]);
+    }
+
+    return () => {};
+  }, [station.stationId, isPlaying, dispatch]);
 
   useEffect(() => {
-    if ('mediaSession' in navigator && state) {
-      navigator.mediaSession.metadata = new MediaMetadata({
-        title: state.name,
-        artwork: [
-          {
-            src: `/images/logo/favicon/icon.jpg`,
-            sizes: '256x256',
-            type: 'image/jpg',
-          },
-          {
-            src: `/images/heroop.webp`,
-            sizes: '512x512',
-            type: 'image/jpg',
-          },
-        ],
-      });
-
-      navigator.mediaSession.setActionHandler('play', function () {
-        dispatch({
-          type: StationReducerActionType.PLAY,
-          payload: { ...state },
-        });
-      });
-      navigator.mediaSession.setActionHandler('pause', function () {
-        dispatch({
-          type: StationReducerActionType.PAUSE,
-        });
-      });
-      navigator.mediaSession.setActionHandler('stop', function () {
-        dispatch({
-          type: StationReducerActionType.PAUSE,
-        });
-      });
+    const storage = localStorage.getItem('likes');
+    if (storage !== null) {
+      const likes: Array<string> = JSON.parse(storage);
+      likes.indexOf(station.stationId) === -1 ? setLike(false) : setLike(true);
     }
-    const spaceBarDown = (e: globalThis.KeyboardEvent) => {
-      window.addEventListener('keydown', (e) => {
-        if (e.code === 'Space' && e.target === document.body) {
-          e.preventDefault();
-        }
-      });
-    };
+  }, [station.stationId]);
 
-    const keyuplistenter = (e: globalThis.KeyboardEvent) => {
-      if (
-        e.code !== 'Space' ||
-        audioRef.current === null ||
-        audioRef.current.currentTime === 0
-      ) {
-        return;
+  useEffect(() => {
+    const storage = localStorage.getItem('likes');
+    if (storage !== null) {
+      const likes: Array<string> = JSON.parse(storage);
+      const stationIndex = likes.includes(station.stationId);
+      if (!like && stationIndex) {
+        const likeSet = new Set([...likes]);
+        likeSet.delete(station.stationId);
+        console.log(likeSet);
+        localStorage.setItem('likes', JSON.stringify([...likeSet]));
       }
-
-      if (e.target == document.body) {
-        e.preventDefault();
+      if (like && !stationIndex) {
+        localStorage.setItem(
+          'likes',
+          JSON.stringify([...likes, station.stationId])
+        );
       }
+    } else {
+      if (station.stationId && like) {
+        localStorage.setItem('likes', JSON.stringify([station.stationId]));
+      }
+    }
+  }, [like, station.stationId]);
 
-      dispatch({
-        type: StationReducerActionType.TOGGLE,
-      });
-
-      return;
-    };
-    addEventListener('keyup', keyuplistenter, { capture: true });
-    addEventListener('keydown', spaceBarDown, { capture: true });
-    return () => {
-      removeEventListener('keyup', keyuplistenter);
-      removeEventListener('keydown', spaceBarDown);
-    };
-  }, [dispatch, state]);
-
-  if (state === undefined) {
-    return <></>;
+  if (station.stationId === '') {
+    return null;
   }
 
-  const { name, favicon, stationurl, countryCode, stationId } = state;
-  const handleAudioLoadStart = (e: React.SyntheticEvent<HTMLAudioElement>) => {
-    if (isPlaying === false) {
-      setAudioStatus({ status: 'paused', message: 'Paused' });
-      return;
-    }
-
-    setAudioStatus({ status: 'loading', message: 'Connecting...' });
-  };
-  const handleAudioPlaying = (e: React.SyntheticEvent<HTMLAudioElement>) => {
-    dispatchStoreStation({
-      type: 'storeStation',
-      payload: stationId,
-    });
-    setAudioStatus({ status: 'playing', message: 'Playing' });
-  };
-  const handleAudioPause = (e: React.SyntheticEvent<HTMLAudioElement>) => {
-    if (e.currentTarget.error) {
-      if (e.currentTarget.error.code === 2) {
-        e.currentTarget.load();
-        dispatch({
-          type: StationReducerActionType.PLAY,
-          payload: { ...state },
-        });
-        return;
-      }
-      setAudioStatus({
-        status: 'error',
-        message: 'An error occured',
-      });
-
-      dispatch({
-        type: StationReducerActionType.PAUSE,
-      });
-      return;
-    }
-    setAudioStatus({ status: 'paused', message: 'Paused' });
-  };
-
   return (
-    <section className='fixed right-0 left-0 bottom-2  z-10 text-CustomTextGrey  grid place-items-center px-2'>
-      <div className='grid grid-cols-[1fr_auto_1fr] justify-between bg-CustomBackgroundBlack/50 backdrop-blur container sm:grid-cols-[1fr_1fr_1fr_auto]  py-1 pl-2 pr-1  gap-6 rounded-md'>
-        <div className='grid grid-cols-[auto_1fr] items-center gap-1 sm:gap-2'>
-          <div className='w-[50px]'>
-            {favicon ? (
-              <picture className='w-[30%]'>
-                <img
-                  src={`/api/image?url=${favicon}`}
-                  alt={'woow'}
-                  width='100%'
-                  className='object-contain rounded'
-                />
-              </picture>
-            ) : (
-              <HiOutlineMusicNote className='w-full h-full svgthin' />
-            )}
-          </div>
-          <div className='overflow-x-hidden overflow-y-auto '>
-            <p className='text-sm  Capitalize whitespace-nowrap sm:overflow-hidden hover:animate-none sm:text-base md:animate-none sm:text-ellipsis w-fit animate-marqueetext'>
-              <span className=''>{name}</span>
-            </p>
-            <div className='hidden overflow-x-hidden sm:grid  sm:grid-cols-[1fr_auto] sm:gap-2'>
-              {getFlagEmoji(countryCode) || '🤷'}
-            </div>
-          </div>
-        </div>
-        <button
-          className='text-5xl self-center justify-self-center grid grid-rows-[auto_auto]  justify-items-center'
-          onClick={() => {
-            dispatch({
-              type: StationReducerActionType.TOGGLE,
-            });
+    <div className='fixed bottom-20 z-20 w-full  text-CustomWhite sm:sticky sm:bottom-0 '>
+      <div className='mx-[10px] bg-CustomLightBlack/80 backdrop-blur-sm p-3 rounded grid grid-cols-[auto_repeat(2,minmax(0,1fr))] gap-x-2 sm:mx-3'>
+        <Image
+          src={
+            station.favicon
+              ? `/api/image?url=${station.favicon}`
+              : '/musicnote.svg'
+          }
+          alt='music note'
+          width={55}
+          height={55}
+          className='object-contain min-w-[55px] min-h-[55px] rounded'
+          onError={(e) => {
+            e.currentTarget.src = '/musicnote.svg';
           }}
-        >
-          <audio
-            src={stationurl}
-            ref={audioRef}
-            onLoadStart={handleAudioLoadStart}
-            onPlaying={handleAudioPlaying}
-            onPause={handleAudioPause}
-            onError={(e) => {
-              e.preventDefault();
-              setAudioStatus({
-                status: 'error',
-                message: 'An error occured',
-              });
-            }}
-          />
-          {isPlaying ? (
-            <HiPause className='fill-CustomActivePurple' />
-          ) : (
-            <HiPlay className='fill-CustomActivePurple  stroke-CustomActivePurple' />
-          )}
-          <span
-            className={`inline-block text-sm ${
-              audioStatus.status === 'error' && 'text-red-600'
-            }`}
-          >
-            {audioStatus.message}
-          </span>
-        </button>
-        <div className='hidden  sm:grid grid-cols-[repeat(2,auto)] items-center justify-center gap-2 sm:justify-self-end'>
-          <HiVolumeUp className='text-3xl' />
-          <input type='range' name='' id='' />
+        />
+        <div className='flex flex-col'>
+          <p>{station.name}</p>
+          <p>{getFlagEmoji(station.countryCode)}</p>
         </div>
-        <button className='justify-self-end sm:self-center sm:justify-self-center'>
-          <HiOutlineHeart className='text-5xl sm:text-4xl svgthin hover:fill-CustomActivePurple hover:stroke-CustomActivePurple' />
-        </button>
-      </div>
-    </section>
-  );
-};
+        <div className='flex justify-end items-center gap-x-2'>
+          <button
+            type='button'
+            aria-label='like Channel'
+            onClick={() => {
+              setLike(!like);
+            }}
+          >
+            {like ? (
+              <HiHeart className='text-5xl text-CustomActive childPath:stroke-1' />
+            ) : (
+              <HiOutlineHeart className='text-5xl text-CustomActive childPath:stroke-1' />
+            )}
 
-export default Player;
+            {/* <HiHeart className='text-5xl text-CustomActive' /> */}
+          </button>
+          <button
+            type='button'
+            aria-label={isPlaying ? 'Pause' : 'Play'}
+            onClick={() => {
+              dispatch({ type: StationReducerActionType.TOGGLE });
+            }}
+          >
+            {isPlaying ? (
+              <HiPause className='text-5xl fill-CustomActive' />
+            ) : (
+              <HiPlay className='text-5xl' />
+            )}
+          </button>
+          <audio
+            src={station.stationurl}
+            ref={audioRef}
+            className='hidden'
+          ></audio>
+        </div>
+      </div>
+    </div>
+  );
+}
