f0259b8084bef9389a4cf78a70d50e66277e9484
Gnm <gmang2236@gmail.com>
2022-11-16 13:15:44 +0300

    fix: add cookie for country code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,15 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { geo, headers } = request;
 
-  return NextResponse.rewrite(
+  const response = NextResponse.rewrite(
     `${request.nextUrl.href}/${geo?.country || 'KE'}`
   );
+  response.cookies.set('code', geo?.country || 'KE');
+  return response;
 }
 
 // See "Matching Paths" below to learn more
