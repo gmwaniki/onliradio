4a0dc3efddd068e1b9a7b5273fddbf9163946c82
Gnm <gmang2236@gmail.com>
2022-11-16 12:39:30 +0300

    refactor:search page

---
+++ src/app/app/search/page.tsx
@@ -0,0 +1,58 @@
+/* 
+  /search?name=Kiss&genre=pop&country=kenya&language=engish
+
+*/
+
+import SearchPage from '../../../components/Search/SearchPage';
+import { getStationsUrl } from '../../../util/getStationsUrl';
+import { getUrl } from '../../../util/getUrl';
+import { TStation } from '../../../util/playableStation';
+
+const getSearchStations = async (
+  url: string,
+  searchParams?: {
+    name: string;
+    genre: string;
+    country: string;
+    language: string;
+  }
+) => {
+  if (searchParams) {
+    const { country, genre, language, name } = searchParams;
+    if (
+      Boolean(country) ||
+      Boolean(genre) ||
+      Boolean(language) ||
+      Boolean(name)
+    ) {
+      const stationsUrl = getStationsUrl(url, {
+        country,
+        genre,
+        language,
+        name,
+      });
+      const results = await fetch(stationsUrl);
+      return results.json() as Promise<TStation[]>;
+    }
+  }
+  return [];
+};
+
+export const revalidate = 0;
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    name: string;
+    genre: string;
+    country: string;
+    language: string;
+  };
+}) {
+  const url = await getUrl();
+
+  const stations = await getSearchStations(url, searchParams);
+
+  return <SearchPage url={url} stations={stations} />;
+}
