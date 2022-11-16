1030f3ca15fb10746139c7b1feb096d9d9b08c56
Gnm <gmang2236@gmail.com>
2022-11-16 12:28:12 +0300

    fix: using app rewrite

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,10 @@ import type { NextRequest } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { geo, headers } = request;
-  // const country = geo?.count
-  const requestHeaders = new Headers(headers);
-  requestHeaders.set('x-code', geo?.country || 'KE');
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
-  return response;
+
+  return NextResponse.rewrite(
+    `${request.nextUrl.href}/${geo?.country || 'KE'}`
+  );
 }
 
 // See "Matching Paths" below to learn more
