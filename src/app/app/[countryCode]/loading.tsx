14f18586aec4941940ca1eb7a043f5fe1a7fdf26
Gnm <gmang2236@gmail.com>
2022-11-18 18:03:22 +0300

    feat: app dash loading page

---
+++ src/app/app/[countryCode]/loading.tsx
@@ -0,0 +1,25 @@
+export default function Loading() {
+  return (
+    <>
+      <div className='h-32 bg-CustomLightBlack/70 pt-2 px-3 rounded animate-pulse'>
+        <div className=' bg-CustomLightBlack/90 h-[55px] rounded mt-4'></div>
+      </div>
+      <div className='flex justify-between animate-pulse mt-4'>
+        <div className='h-4 w-1/3 bg-CustomLightBlack/70 rounded'></div>
+        <div className='h-4 w-1/6 bg-CustomLightBlack/70 rounded'></div>
+      </div>
+      <div className='mt-4 h-48 bg-CustomLightBlack/70 rounded animate-pulse'></div>
+      <div className='flex justify-between animate-pulse mt-4'>
+        <div className='h-4 w-1/3 bg-CustomLightBlack/70 rounded'></div>
+        <div className='h-4 w-1/6 bg-CustomLightBlack/70 rounded'></div>
+      </div>
+      <div className='mt-4 animate-pulse flex gap-x-4 justify-between'>
+        <div className='w-[150px] h-[160px] bg-CustomLightBlack/70 rounded'></div>
+        <div className='w-[150px] h-[160px] bg-CustomLightBlack/70 rounded'></div>
+        <div className='w-[150px] h-[160px] bg-CustomLightBlack/70 rounded'></div>
+        <div className='w-[150px] h-[160px] bg-CustomLightBlack/70 rounded'></div>
+        <div className='w-[150px] h-[160px] bg-CustomLightBlack/70 rounded'></div>
+      </div>
+    </>
+  );
+}
