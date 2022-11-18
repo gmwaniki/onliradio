338117fb338ddbc853cf2c586912ff73641604bb
Gnm <gmang2236@gmail.com>
2022-11-19 01:02:57 +0300

    fix: use rewrites - This avoid page refreshes causing station to stop playing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,13 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
+// // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const { geo, headers } = request;
+  const { geo } = request;
 
-  const requestHeaders = new Headers(headers);
-  requestHeaders.set('x-code', geo?.country || 'KE');
-
-  // const response = NextResponse.rewrite(
-  //   `${request.nextUrl.href}/${geo?.country || 'KE'}`,
-  //   { request: { headers: requestHeaders } }
-  // );
-
-  // return response;
-  return NextResponse.next({ request: { headers: requestHeaders } });
+  if (request.nextUrl.pathname === '/app') {
+    return NextResponse.rewrite(
+      `${request.nextUrl.origin}/app/${geo?.country || 'KE'}`
+    );
+  }
 }
-
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ['/app/:path*', '/'],
-};
