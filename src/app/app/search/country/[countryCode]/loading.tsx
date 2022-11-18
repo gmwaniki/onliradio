5e61a75554b1ca945a36649f420b14bd461ee514
Gnm <gmang2236@gmail.com>
2022-11-18 12:02:48 +0300

    feat:Loading component for app page

---
+++ src/app/app/search/country/[countryCode]/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div>
+      <div className=' h-10 bg-red-600'></div>
+      <div className='h-6 bg-green-500'></div>
+      <div className='flex'>
+        <div className='h-40 w-28 bg-blue-600'></div>
+      </div>
+    </div>
+  );
+}
