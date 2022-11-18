8fc09ac9a78f18d6b30b5f8058e358d3774c9aad
Gnm <gmang2236@gmail.com>
2022-11-18 18:03:40 +0300

    feat: search page loading component

--- src/app/app/search/loading.tsx
+++ src/app/app/search/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <div className='text-CustomWhite'>Loading .....</div>;
+  return (
+    <div className='h-32 bg-CustomLightBlack/70 pt-2 px-3 rounded animate-pulse'>
+      <div className=' bg-CustomLightBlack/90 h-[55px] rounded mt-4'></div>
+    </div>
+  );
 }
