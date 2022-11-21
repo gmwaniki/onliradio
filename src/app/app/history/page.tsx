64ddc64367c9db5408083eaa2619a38a3708f1ed
Gnm <gmang2236@gmail.com>
2022-11-21 10:01:22 +0300

    feat: history Page

--- src/app/app/history/page.tsx
+++ src/app/app/history/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-  return <div>History</div>;
+import HistoryPage from '../../../components/History/HistoryPage';
+import { getUrl } from '../../../util/getUrl';
+
+export default async function Page() {
+  const url = await getUrl();
+
+  return <HistoryPage url={url} />;
 }
