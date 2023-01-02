16900efb5419b082d00486a98a68501eaa611c71
gmwaniki <gmang2236@gmail.com>
2023-01-02 07:34:57 +0100

    Linting

--- src/util/getImage.ts
+++ src/util/getImage.ts
@@ -1,8 +1,9 @@
 // import { IncomingMessage, request } from 'node:http';
 // import { request as requestHttps } from 'node:https';
-import type { URL } from 'node:url';
-import { http, https } from 'follow-redirects';
 import { IncomingMessage } from 'node:http';
+import type { URL } from 'node:url';
+
+import { http, https } from 'follow-redirects';
 const { request } = http;
 const { request: requestHttps } = https;
 
