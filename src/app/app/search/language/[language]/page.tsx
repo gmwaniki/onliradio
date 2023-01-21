22e18d2a97e2abc53c065ec7119c7fe0e70a3ed8
gmwaniki <gmang2236@gmail.com>
2023-01-21 20:46:50 +0100

    moved Search component to layout

--- src/app/app/search/language/[language]/page.tsx
+++ src/app/app/search/language/[language]/page.tsx
@@ -1,4 +1,3 @@
-import Search from '../../../../../components/Search/Search';
 import Station from '../../../../../components/Station/Station';
 import { getUrl } from '../../../../../util/getUrl';
 import { TStation } from '../../../../../util/playableStation';
@@ -29,7 +28,6 @@ export default async function Page({
 
   return (
     <section className='text-CustomWhite'>
-      <Search />
       <h1 className='text-center font-semibold text-2xl py-2'>
         Stations in {params.language}
       </h1>
