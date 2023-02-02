17caf82f54f97af54b2b539e94b7e7fcc2a2f9ee
gmwaniki <gmang2236@gmail.com>
2023-02-02 21:12:10 +0100

    fix styling and make component reusable

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
-import React from "react";
-import { HiOutlineChevronRight } from "react-icons/hi";
+import Link from 'next/link';
+import React from 'react';
+import { HiOutlineChevronRight } from 'react-icons/hi';
 
-import { TCountry } from "../../app/app/countries/page";
-import getFlagEmoji from "../../util/getFlagEmoji";
+import { TCountry } from '../../app/app/countries/page';
+import getFlagEmoji from '../../util/getFlagEmoji';
 
 type TCountryProps = {
   country: TCountry;
@@ -12,23 +12,23 @@ type TCountryProps = {
 const Country = (prop: TCountryProps) => {
   const { country } = prop;
   return (
-    <li className="sm:aspect-[2/.5] 2xl:aspect-[4/2]">
-      <Link
-        href={`/app/search/country/${country.iso_3166_1}`}
-        className="flex justify-between items-center pr-4 pl-2 py-2 mb-4 rounded bg-CustomLightBlack/50  relative isolate sm:mb-0 h-full"
-      >
-        <div className=" flex flex-col ">
-          <p>
-            {country.name}
-            {getFlagEmoji(country.iso_3166_1) || ""}
-          </p>
-        </div>
-        <HiOutlineChevronRight className="text-7xl childPath:stroke-1" />
-        <span className="absolute -z-10 top-1/2 -translate-y-1/2 right-0 blur-lg text-7xl">
-          {getFlagEmoji(country.iso_3166_1) || ""}
-        </span>
-      </Link>
-    </li>
+    <Link
+      href={`/app/search/country/${country.iso_3166_1}`}
+      className='group grid grid-cols-[1fr,auto] items-center pr-4 pl-2 py-2 mb-4 rounded bg-CustomLightBlack/50  relative isolate sm:mb-0 h-full'
+    >
+      <div className=' flex flex-col flex-grow'>
+        <p className='grid grid-cols-[auto,1fr] items-center gap-x-1 '>
+          <span className='basis-auto'> {country.name}</span>
+          <span className='justify-self-end'>
+            {getFlagEmoji(country.iso_3166_1) || ''}
+          </span>
+        </p>
+      </div>
+      <HiOutlineChevronRight className='text-7xl childPath:stroke-1' />
+      <span className='absolute -z-10 top-1/2 -translate-y-1/2 right-0 blur-lg text-7xl transition-transform duration-500 group-hover:scale-150 group-hover:transition-transform group-hover:duration-200'>
+        {getFlagEmoji(country.iso_3166_1) || ''}
+      </span>
+    </Link>
   );
 };
 
