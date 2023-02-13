80f6939ff613dea4ae8ed8c2053c05844f4faed0
gmwaniki <gmang2236@gmail.com>
2023-02-13 11:43:07 +0100

    add Loading component for countries

---
+++ src/components/Country/LoadCountries.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+
+import { wait } from "../../util/wait";
+
+type LoadCountriesProps = {
+  getNext: () => void;
+  isFetching: boolean;
+  isFetched: boolean;
+};
+
+const LoadCountries = ({
+  getNext,
+  isFetching,
+  isFetched,
+}: LoadCountriesProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const refIntersecting = useRef<boolean>(false);
+  useEffect(() => {
+    let options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && !refIntersecting.current) {
+        refIntersecting.current = true;
+        getNext();
+        wait(1000, () => (refIntersecting.current = false));
+      }
+    }, options);
+    observer.observe(ref.current!!);
+    return () => {
+      observer.disconnect;
+    };
+  }, [getNext, isFetching]);
+
+  return (
+    <div ref={ref} className="text-center">
+      {isFetching ? "Loading..." : null}
+    </div>
+  );
+};
+
+export default LoadCountries;
