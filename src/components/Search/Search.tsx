cc4a4170a267d017af8208411adb376986a1cc1a
gmwaniki <gmang2236@gmail.com>
2023-02-16 13:12:19 +0100

    add linting

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,42 +1,42 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { HiOutlineSearch } from 'react-icons/hi';
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
 
 export default function Search({ children }: { children?: React.ReactNode }) {
   const router = useRouter();
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
   const onSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
     router.push(`/app/search?name=${searchValue}`);
   };
   return (
-    <div className='bg-CustomLightBlack rounded sticky top-1 z-10 sm:bg-CustomLightBlack/80 sm:backdrop-blur-sm sm:top-[10px]'>
-      <form className='flex px-3 pb-3 pt-2' onSubmit={onSubmit}>
-        <div className='grid grid-cols-[auto,min-content] auto-rows-auto flex-1'>
+    <div className="bg-CustomLightBlack rounded sticky top-1 z-10 sm:bg-CustomLightBlack/80 sm:backdrop-blur-sm sm:top-[10px]">
+      <form className="flex px-3 pb-3 pt-2" onSubmit={onSubmit}>
+        <div className="grid grid-cols-[auto,min-content] auto-rows-auto flex-1">
           <label
-            htmlFor='name'
-            className='text-CustomWhite font-medium col-span-2'
+            htmlFor="name"
+            className="text-CustomWhite font-medium col-span-2"
           >
             Station Name
           </label>
           <input
-            type='text'
-            name='station_name'
-            id='name'
-            placeholder='Name'
-            className='py-3 pl-2 rounded-l text-lg text-white bg-[#6C6C6C] focus:outline-none focus:ring-2 focus:ring-CustomActive  focus:ring-inset '
+            type="text"
+            name="station_name"
+            id="name"
+            placeholder="Name"
+            className="py-3 pl-2 rounded-l text-lg text-white bg-[#6C6C6C] focus:outline-none focus:ring-2 focus:ring-CustomActive  focus:ring-inset "
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               setSearchValue(e.target.value);
             }}
           />
           <button
-            type='submit'
-            aria-label='Search'
-            className='bg-CustomActive rounded-r w-12 flex justify-center items-center'
+            type="submit"
+            aria-label="Search"
+            className="bg-CustomActive rounded-r w-12 flex justify-center items-center"
           >
-            <HiOutlineSearch className='text-3xl text-CustomWhite' />
+            <HiOutlineSearch className="text-3xl text-CustomWhite" />
           </button>
         </div>
       </form>
