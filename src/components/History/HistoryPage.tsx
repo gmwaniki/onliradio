74e16a98373fd6839a1f23b10a9dbe59c49e178f
gmwaniki <gmang2236@gmail.com>
2023-02-22 13:16:00 +0100

    Linting fix

--- src/components/History/HistoryPage.tsx
+++ src/components/History/HistoryPage.tsx
@@ -1,17 +1,17 @@
-'use client';
-import { useEffect, useState } from 'react';
-import { TStation } from '../../util/playableStation';
-import Station from '../Station/Station';
+"use client";
+import { useEffect, useState } from "react";
+import { TStation } from "../../util/playableStation";
+import Station from "../Station/Station";
 
 type TProps = {
   url: string;
 };
 
-export default function HistoryPage({ url }: TProps) {
+export default function HistoryStations({ url }: TProps) {
   const [stations, setStations] = useState<TStation[]>([]);
 
   useEffect(() => {
-    const likedStations = localStorage.getItem('history');
+    const likedStations = localStorage.getItem("history");
     const getStations = async () => {
       if (likedStations === null) {
         return;
@@ -19,7 +19,7 @@ export default function HistoryPage({ url }: TProps) {
       const stationIds: string[] = JSON.parse(likedStations);
       try {
         const result = await fetch(
-          `${url}/stations/byuuid?uuids=${stationIds.join(',')}`
+          `${url}/stations/byuuid?uuids=${stationIds.join(",")}`,
         );
         const resultStations = (await result.json()) as TStation[];
         setStations(resultStations);
@@ -30,20 +30,14 @@ export default function HistoryPage({ url }: TProps) {
     getStations();
   }, [url]);
   return (
-    <section className='text-CustomWhite'>
-      <h1 className='text-center font-semibold text-4xl mb-4'>
-        Recently Played
-      </h1>
-
-      <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)]  items-center justify-center  gap-y-4 gap-x-12 '>
-        {stations.map((station) => {
-          return (
-            <li key={station.stationuuid}>
-              <Station station={station} />
-            </li>
-          );
-        })}
-      </ul>
-    </section>
+    <>
+      {stations.map((station) => {
+        return (
+          <li key={station.stationuuid}>
+            <Station station={station} />
+          </li>
+        );
+      })}
+    </>
   );
 }
