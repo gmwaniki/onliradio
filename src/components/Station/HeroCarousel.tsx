d9c9aff899fea3890de687e18eaeb82b471e537b
gmwaniki <gmang2236@gmail.com>
2023-02-24 21:15:06 +0100

    fix carousel freeze by adding a throttle

--- src/components/Station/HeroCarousel.tsx
+++ src/components/Station/HeroCarousel.tsx
@@ -11,9 +11,11 @@ type TProps = {
 };
 export default function HeroCarousel({ stations }: TProps) {
   const [index, setIndex] = useState(0);
+  const [isDisable, setisDisabled] = useState(false);
   const station = stations[index];
 
   const incrementIndex = () => {
+    setisDisabled(true);
     const nextStation = stations[index + 1];
     if (nextStation === undefined) {
       setIndex(0);
@@ -23,9 +25,13 @@ export default function HeroCarousel({ stations }: TProps) {
     setIndex((index) => index + 1);
   };
   const decrementIndex = () => {
+    setisDisabled(true);
     const nextStation = stations[index - 1];
     if (nextStation === undefined) {
       setIndex(stations.length - 1);
+      // wait(30, function () {
+      //   setisDisabled((prev) => false);
+      // });
       return;
     }
 
@@ -33,17 +39,24 @@ export default function HeroCarousel({ stations }: TProps) {
   };
 
   return (
-    <div className='text-CustomWhite pt-2'>
-      <div className='relative scrollbar grid grid-rows-[min-content,1fr]  grid-cols-[.3fr,1fr,.3fr] lg:grid-rows-1  lg:gap-5'>
+    <div className='text-CustomWhite mt-4'>
+      <div className='relative scrollbar grid grid-rows-[min-content,1fr]  grid-cols-[.2fr,minmax(0,1fr),.2fr] lg:grid-rows-1  lg:gap-5'>
         <m.button
           onClick={decrementIndex}
           className=' absolute left-5 z-10 self-center justify-self-center bg-CustomBlack border border-CustomWhite rounded-full p-2 lg:static lg:bg-CustomLightBlack lg:p-5 lg:justify-self-end lg:border-0'
           whileHover={{ scale: 1.25 }}
+          disabled={isDisable}
+          aria-label='Previous'
+          aria-disabled={isDisable}
         >
           <HiChevronLeft className='text-3xl' />
         </m.button>
         <div className='col-span-full col-start-1 row-start-2 lg:col-span-1 lg:col-start-2 lg:row-start-1 '>
-          <AnimatePresence initial={false} mode='wait'>
+          <AnimatePresence
+            initial={false}
+            mode='wait'
+            onExitComplete={() => setisDisabled(false)}
+          >
             <HeroStation station={station} key={station.stationuuid} />
           </AnimatePresence>
         </div>
@@ -51,6 +64,9 @@ export default function HeroCarousel({ stations }: TProps) {
           onClick={incrementIndex}
           className='absolute right-5 z-10 col-start-3 self-center justify-self-center bg-CustomBlack border border-CustomWhite  rounded-full p-2 lg:static lg:bg-CustomLightBlack lg:p-5 lg:justify-self-start lg:border-0'
           whileHover={{ scale: 1.25 }}
+          disabled={isDisable}
+          aria-label='Next'
+          aria-disabled={isDisable}
         >
           <HiChevronRight className='text-3xl ' />
         </m.button>
