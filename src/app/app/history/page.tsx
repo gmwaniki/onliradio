f915d4b06c6bde85d7c19bfde3b091274e7c4a34
gmwaniki <gmang2236@gmail.com>
2023-03-16 13:41:17 +0100

    Add hitory page

--- src/app/app/history/page.tsx
+++ src/app/app/history/page.tsx
@@ -1,8 +1,14 @@
-import HistoryPage from '../../../components/History/HistoryPage';
+import HistoryStations from '../../../components/History/HistoryPage';
 import { getUrl } from '../../../util/getUrl';
 
 export default async function Page() {
   const url = await getUrl();
 
-  return <HistoryPage url={url} />;
+  return (
+    <section className='text-CustomWhite'>
+      <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)]  items-center justify-center  gap-y-4 gap-x-12 '>
+        <HistoryStations url={url} />
+      </ul>
+    </section>
+  );
 }
