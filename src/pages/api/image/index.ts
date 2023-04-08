79856238ed067c35c6c8ab13b10f9eacc06c5f56
gmwaniki <gmang2236@gmail.com>
2023-04-08 19:05:58 +0200

    set up default images

--- src/pages/api/image/index.ts
+++ src/pages/api/image/index.ts
@@ -1,63 +1,39 @@
+import fs from 'node:fs';
+
 import { NextApiRequest, NextApiResponse } from 'next';
 import sharp from 'sharp';
-
-import { requestImage } from '../../../util/getImage';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let { url } = req.query;
-
-  if (!url || Array.isArray(url)) {
-    res.status(400);
-    res.end();
-    return;
-  }
+  const { url } = req.query;
 
   try {
+    if (!url || Array.isArray(url)) {
+      throw new Error('Invalid url');
+    }
+
     const imageUrl = new URL(url);
-    const imageResponse = await requestImage(imageUrl);
-    const imageType = imageResponse.headers['content-type'];
-
-    const imageSize = imageResponse.headers['content-length'];
-
-    if (!imageType) {
-      return res.status(500);
-    }
-    if (!imageType.includes('image')) {
-      return res.status(400);
-    }
-
-    if (imageSize && parseInt(imageSize) <= 1000) {
-      res.setHeader('Content-Type', imageType);
-      return imageResponse.pipe(res);
-    }
-
-    const smallerimage = sharp()
+    const response = await fetch(imageUrl);
+    const responseImageBuffer = await response.arrayBuffer();
+    const sharpPipe = sharp(responseImageBuffer);
+    const newImage = await sharpPipe
       .resize(100, 100, {
         fit: 'outside',
         width: 100,
         height: 100,
         background: { r: 0, g: 0, b: 0, alpha: 0 },
       })
-      .png({ quality: 5, compressionLevel: 9 });
+      .png({ quality: 5, compressionLevel: 9 })
+      .toBuffer();
 
     res.setHeader('Content-Type', 'image/png');
-    res.setHeader('Cache-Control', 'max-age=31536000, immutable');
-
-    return imageResponse
-      .pipe(smallerimage)
-      .on('error', (error) => {
-        console.log(error);
-        res.status(400);
-        res.end();
-      })
-      .pipe(res);
+    res.send(newImage);
   } catch (error) {
-    console.log(error);
-    return res.status(500);
+    const defaultImage = fs.createReadStream('public/musicnote.svg');
+    res.setHeader('Content-Type', 'image/svg+xml');
+    defaultImage.pipe(res);
   }
-
-  //   res.status(200).json({ name: 'John Doe' });
 }
+export const runtime = 'nodejs';
