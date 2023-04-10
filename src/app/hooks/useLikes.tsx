332ae856f0ad0d67e4ab82370e0ef47721aa8514
gmwaniki <gmang2236@gmail.com>
2023-04-10 15:06:05 +0200

    Feat likes hook

---
+++ src/app/hooks/useLikes.tsx
@@ -0,0 +1,65 @@
+'use client';
+import { useMemo, useState } from 'react';
+
+type T1 = {
+  isliked: boolean;
+  like: (stationId: string) => void;
+  unlike: (stationId: string) => void;
+};
+type T2 = {
+  likes: Record<string, string>;
+};
+
+type TLikes<T extends string | null> = T extends string ? T1 : T2;
+
+const getLikes = () => {
+  const likes = localStorage.getItem('likes');
+  if (!likes) return null;
+  const likedStations: Record<string, string> = JSON.parse(likes);
+  return likedStations;
+};
+function useLikes<T extends string | null>(stationId?: T): TLikes<T> {
+  const [likes, setLikes] = useState(() => {
+    return getLikes() || null;
+  });
+
+  const isliked = useMemo(() => {
+    if (stationId && likes && likes[stationId] === '1') return true;
+    return false;
+  }, [likes, stationId]);
+
+  if (stationId === null) {
+    return {
+      likes,
+    } as TLikes<T>;
+  }
+  const like = (stationId: string) => {
+    // const likedStations = getLikes();
+    if (!likes) {
+      localStorage.setItem('likes', JSON.stringify({ [stationId]: '1' }));
+      return;
+    }
+    localStorage.setItem(
+      'likes',
+      JSON.stringify({ ...likes, [stationId]: '1' })
+    );
+    setLikes({ ...likes, [stationId]: '1' });
+  };
+  const unlike = (stationId: string) => {
+    // const likedStations = getLikes();
+    if (!likes) {
+      localStorage.setItem('likes', JSON.stringify({ [stationId]: '0' }));
+      return;
+    }
+
+    localStorage.setItem(
+      'likes',
+      JSON.stringify({ ...likes, [stationId]: '0' })
+    );
+    // setLikes((prev) => ({ ...prev, [stationId]: '0' }));
+  };
+
+  return { likes, like, unlike, isliked } as TLikes<T>;
+}
+
+export default useLikes;
