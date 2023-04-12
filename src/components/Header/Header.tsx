f4a1a83d4d82f5e3751364125d10367550063468
gmwaniki <gmang2236@gmail.com>
2023-04-12 19:02:42 +0200

    clean up of unused variables and fix linting

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-import { HiMenuAlt3, HiX } from "react-icons/hi";
-import logo from "../../assets/logo.svg";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { HiMenuAlt3, HiX } from 'react-icons/hi';
+
+import logo from '../../assets/logo.svg';
 
 const Header = () => {
   const [menu, setMenu] = useState<boolean>(false);
@@ -12,61 +13,61 @@ const Header = () => {
   }
   return (
     <>
-      <header className=" grid grid-cols-2 sm:text-lg sm:grid-cols-[.5fr_2.5fr]  ">
-        <Link href="/">
-          <span className="sr-only">Homepage</span>
-          <Image src={logo} alt="onliradio logo" />
+      <header className=' grid grid-cols-2 sm:text-lg sm:grid-cols-[.5fr_2.5fr]  '>
+        <Link href='/'>
+          <span className='sr-only'>Homepage</span>
+          <Image src={logo} alt='onliradio logo' />
         </Link>
-        <div className="justify-self-end grid place-items-center sm:hidden">
-          <button onClick={handleMenuClick} className="text-3xl ">
-            <span className="sr-only">Menu</span>
+        <div className='justify-self-end grid place-items-center sm:hidden'>
+          <button onClick={handleMenuClick} className='text-3xl '>
+            <span className='sr-only'>Menu</span>
             {menu ? (
-              <HiX data-testid="closemenu" />
+              <HiX data-testid='closemenu' />
             ) : (
-              <HiMenuAlt3 data-testid="hamburger" />
+              <HiMenuAlt3 data-testid='hamburger' />
             )}
           </button>
         </div>
-        <nav className="hidden sm:grid sm:grid-cols-[1fr_auto]">
-          <ul className="grid grid-flow-col items-center justify-center gap-8">
+        <nav className='hidden sm:grid sm:grid-cols-[1fr_auto]'>
+          <ul className='grid grid-flow-col items-center justify-center gap-8'>
             <li>
               <Link
-                href="/"
-                className="relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full"
+                href='/'
+                className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'
               >
                 Home
               </Link>
             </li>
             <li>
               <Link
-                href="/"
-                className="relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full"
+                href='/'
+                className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'
               >
                 Contact us
               </Link>
             </li>
             <li>
               <Link
-                href="/"
-                className="relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full"
+                href='/'
+                className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'
               >
                 Faq
               </Link>
             </li>
           </ul>
-          <ul className="grid grid-flow-col items-center gap-8">
+          <ul className='grid grid-flow-col items-center gap-8'>
             <li>
               <Link
                 href={`/app`}
-                className="bg-gradient text-white py-[10px] px-[30px] rounded-md transition-transform duration-300 hover:opacity-90 "
+                className='bg-gradient text-white py-[10px] px-[30px] rounded-md transition-transform duration-300 hover:opacity-90 '
               >
                 Listen
               </Link>
             </li>
             <li>
               <Link
-                href="/"
-                className="relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full"
+                href='/'
+                className='relative transition-all hover:text-black text-slate-500 after:absolute after:h-1 after:w-0 after:bg-gradient after:left-0 after:-bottom-1 after:z-10  after:transition-[width] after:duration-300 hover:after:w-full'
               >
                 Sign up
               </Link>
