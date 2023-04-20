4d92d92925589654e9523cb45fbc9fc476dd677b
gmwaniki <gmang2236@gmail.com>
2023-04-20 14:59:51 +0200

    Feat audio support hook

---
+++ src/app/hooks/useAudio.tsx
@@ -0,0 +1,53 @@
+import Hls from 'hls.js';
+import React, { useContext, useEffect, useState } from 'react';
+
+import { AudioContext } from '../providers/AudioContext';
+
+const useAudio = (audioRef: React.RefObject<HTMLAudioElement>) => {
+  const { dispatch, state } = useContext(AudioContext);
+  const [error, setError] = useState('');
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+    const { isPlaying, station } = state;
+    const play = () => {
+      if (!audioElement) return;
+
+      if (station.hls === 0) {
+        audioElement.load();
+        audioElement.play().catch((error) => {
+          setError('Unable to play track');
+        });
+      } else {
+        const hlsPlayback = new Hls();
+        if (audioElement.canPlayType('application/vnd.apple.mpegurl')) {
+          audioElement.onloadedmetadata = (e) => {
+            audioElement.play();
+          };
+        }
+        hlsPlayback.loadSource(station.stationurl);
+
+        hlsPlayback.attachMedia(audioElement);
+        hlsPlayback.on(Hls.Events.MANIFEST_PARSED, () => {
+          audioElement.play();
+        });
+      }
+      setIsAudioPlaying(true);
+    };
+    const pause = () => {
+      if (!audioElement) return;
+      audioElement.pause();
+      setIsAudioPlaying(false);
+    };
+    if (state.isPlaying) {
+      play();
+    } else {
+      pause();
+    }
+  }, [audioRef, dispatch, state]);
+
+  return { isAudioPlaying, error };
+};
+
+export default useAudio;
