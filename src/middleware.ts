5031cb39f1912d9f1535fe5c0428f3f93cefe532
gmwaniki <gmang2236@gmail.com>
2023-04-24 19:22:45 +0200

    add csp to upgrade http requests

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,38 @@ import { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const { geo } = request;
 
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-nonce', nonce);
+  requestHeaders.set(
+    'Content-Security-Policy',
+    // Replace newline characters and spaces
+    cspHeader.replace(/\s{2,}/g, ' ').trim()
+  );
+
   const rewriteUrl = new URL(`/app/${geo?.country || 'no'}`, request.url);
 
   if (request.nextUrl.pathname === '/app') {
-    return NextResponse.rewrite(rewriteUrl);
+    return NextResponse.rewrite(rewriteUrl, { headers: requestHeaders });
   }
+  return NextResponse.next({
+    headers: requestHeaders,
+    request: {
+      headers: requestHeaders,
+    },
+  });
 }
