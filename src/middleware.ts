1ddcfeba3053825160a36d31388879c9cab92718
gmwaniki <gmang2236@gmail.com>
2023-04-29 19:31:04 +0200

    fix csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,17 +6,7 @@ export function middleware(request: NextRequest) {
   const { geo } = request;
 
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
-  const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
+  const cspHeader = `    
     upgrade-insecure-requests;
 `;
 
