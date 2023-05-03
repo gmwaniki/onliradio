3d953a28dc79d97b7d438b8c3a87848b26ed58cc
gmwaniki <gmang2236@gmail.com>
2023-05-03 02:04:32 +0200

    fix styling and use conditional rendering

--- src/components/Search/SearchPage.tsx
+++ src/components/Search/SearchPage.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
+
 import { TStation } from '../../util/playableStation';
 import Station from '../Station/Station';
+import AdvancedSearch from './AdvancedSearch';
 
 type TProps = {
   url: string;
@@ -15,17 +18,25 @@ type TProps = {
 };
 
 export default function SearchPage({ stations }: TProps) {
+  const searchParams = useSearchParams();
   return (
     <section className='text-CustomWhite '>
-      <ul className='mt-4 grid grid-flow-row grid-cols-[repeat(auto-fit,150px)] items-center justify-center gap-y-5 gap-x-3 lg:grid-cols-[repeat(3,minmax(275px,1fr))] '>
-        {stations.map((station) => {
-          return (
-            <li key={station.stationuuid}>
-              <Station station={station} />
-            </li>
-          );
-        })}
-      </ul>
+      <AdvancedSearch />
+      {stations.length === 0 && searchParams?.size > 0 ? (
+        <p className='text-center text-2xl pt-5 font-semibold'>
+          No result found
+        </p>
+      ) : (
+        <ul className='mt-4 grid grid-flow-row grid-cols-[repeat(auto-fit,150px)] items-center justify-center gap-y-5 gap-x-3 lg:grid-cols-[repeat(3,minmax(275px,1fr))] '>
+          {stations.map((station) => {
+            return (
+              <li key={station.stationuuid}>
+                <Station station={station} />
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </section>
   );
 }
