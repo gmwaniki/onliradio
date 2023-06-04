09e34112b73f4d98544fbc265097a08c993f1cff
gmwaniki <gmang2236@gmail.com>
2023-06-05 01:58:01 +0200

    remove extra links and fix styling

--- src/app/app/[countryCode]/page.tsx
+++ src/app/app/[countryCode]/page.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link';
 import { HiOutlineArrowRight } from 'react-icons/hi';
 
 import Country from '../../../components/Country/Country';
+import Search from '../../../components/Search/Search';
 import HeroCarousel from '../../../components/Station/HeroCarousel';
 import Station from '../../../components/Station/Station';
+import { getCountryName } from '../../../util/getCountries';
 import { getUrl } from '../../../util/getUrl';
 import { TStation } from '../../../util/playableStation';
 import { TCountry } from '../countries/page';
@@ -17,19 +19,19 @@ const getMostVotedStations = async (url: string) => {
 
 const getLocalStations = async (url: string, countryCode: string) => {
   const results = await fetch(
-    `${url}/stations/search?order=votes&countrycode=${countryCode}&hidebroken=true&is_https=true&limit=5&reverse=true`
+    `${url}/stations/search?order=votes&countrycode=${countryCode}&hidebroken=true&is_https=true&limit=6&reverse=true`
   );
   return results.json() as Promise<TStation[]>;
 };
 const getMostPlayedStations = async (url: string) => {
   const results = await fetch(
-    `${url}/stations/search?order=clickcount&hidebroken=true&is_https=true&limit=5&reverse=true`
+    `${url}/stations/search?order=clickcount&hidebroken=true&is_https=true&limit=6&reverse=true`
   );
   return results.json() as Promise<TStation[]>;
 };
 const getCountries = async (url: string) => {
   const results = await fetch(
-    `${url}/countries?order=stationcount&reverse=true&limit=5`
+    `${url}/countries?order=stationcount&reverse=true&limit=6`
   );
   return results.json() as Promise<TCountry[]>;
 };
@@ -48,67 +50,67 @@ export default async function Page({
 
   return (
     <>
-      <HeroCarousel stations={stations} />
-      <div className='grid grid-flow-row auto-rows-min  gap-y-5'>
-        <div className='text-CustomWhite'>
-          <div className='flex justify-between items-center py-2'>
-            <p className='text-lg font-medium'>Top Stations in your Area</p>
-            <Link
-              href={`/app/search/country/${countryCode}`}
-              className='flex items-center gap-x-2'
-            >
-              View More
-              <HiOutlineArrowRight />
-            </Link>
+      <Search />
+      <div className='pt-4 '>
+        <div>
+          <HeroCarousel stations={stations} />
+          <div className='grid grid-flow-row auto-rows-min  gap-y-5'>
+            <div className='text-CustomWhite'>
+              <div className='flex justify-between items-center py-2'>
+                <p className='text-lg  font-bold md:text-2xl'>
+                  Top Stations in{' '}
+                  {getCountryName.of(countryCode.toLocaleUpperCase()) ||
+                    'your Area'}
+                </p>
+              </div>
+              <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)]  gap-4 justify-center  lg:pl-3 lg:grid-cols-[repeat(auto-fit,minmax(275px,1fr))] lg:justify-between '>
+                {localStations.map((station) => {
+                  return (
+                    <li key={station.stationuuid} className='h-full'>
+                      <Station station={station} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className='text-CustomWhite'>
+              <div className='flex justify-between items-center py-2'>
+                <p className='text-lg font-bold md:text-2xl'>
+                  Most Played Stations
+                </p>
+              </div>
+              <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)]  gap-4 justify-center  lg:pl-3 lg:grid-cols-[repeat(auto-fit,minmax(275px,1fr))] lg:justify-between'>
+                {mostPlayedStations.map((station) => {
+                  return (
+                    <li key={station.stationuuid}>
+                      <Station station={station} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className='text-CustomWhite'>
+              <div className='flex justify-between items-center py-2'>
+                <p className='text-lg font-bold md:text-2xl'>Countries</p>
+                <Link
+                  href='/app/countries'
+                  className='flex items-center gap-x-2 sm:hidden'
+                >
+                  View More
+                  <HiOutlineArrowRight />
+                </Link>
+              </div>
+              <ul className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] auto-rows-[minmax(125px,1fr)]  gap-4 sm:pl-3'>
+                {countries.map((country) => {
+                  return (
+                    <li key={country.iso_3166_1}>
+                      <Country country={country} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
-          <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)]  gap-4 justify-center  lg:pl-3 lg:grid-cols-[repeat(auto-fill,minmax(275px,1fr))] lg:justify-between '>
-            {localStations.map((station) => {
-              return (
-                <li key={station.stationuuid} className='h-full'>
-                  <Station station={station} />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div className='text-CustomWhite'>
-          <div className='flex justify-between items-center py-2'>
-            <p className='text-lg font-medium'>Most Played Stations</p>
-            <Link
-              href='/app/search/topclick'
-              className='flex items-center gap-x-2'
-            >
-              View More
-              <HiOutlineArrowRight />
-            </Link>
-          </div>
-          <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)]  gap-4 justify-center  lg:pl-3 lg:grid-cols-[repeat(auto-fill,minmax(275px,1fr))] lg:justify-between'>
-            {mostPlayedStations.map((station) => {
-              return (
-                <li key={station.stationuuid}>
-                  <Station station={station} />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div className='text-CustomWhite'>
-          <div className='flex justify-between items-center py-2'>
-            <p className='text-lg font-medium'>Countries</p>
-            <Link href='/app/countries' className='flex items-center gap-x-2'>
-              View More
-              <HiOutlineArrowRight />
-            </Link>
-          </div>
-          <ul className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] auto-rows-[minmax(125px,1fr)]  gap-4 sm:pl-3'>
-            {countries.map((country) => {
-              return (
-                <li key={country.iso_3166_1}>
-                  <Country country={country} />
-                </li>
-              );
-            })}
-          </ul>
         </div>
       </div>
     </>
