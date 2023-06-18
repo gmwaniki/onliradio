5da01284e180acaee3cf403b69870c49fb076882
gmwaniki <gmang2236@gmail.com>
2023-06-19 01:58:38 +0200

    remove extra links and fix styling

--- src/app/app/favourites/page.tsx
+++ src/app/app/favourites/page.tsx
@@ -6,9 +6,7 @@ export default async function Page() {
 
   return (
     <section className='text-CustomWhite'>
-      <h1 className='text-center font-semibold text-4xl mb-4'>
-        Stations you Liked
-      </h1>
+      <h1 className='text-center font-semibold text-4xl mb-4'>Favourites</h1>
       <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)]  items-center justify-center  gap-y-4 gap-x-12 lg:grid-cols-[repeat(3,minmax(250px,1fr))]'>
         <FavouritesPage url={url} />
       </ul>
