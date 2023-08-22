49fb23bd65dcce7f00305e689e49d7d7071b702a
gmwaniki <gmang2236@gmail.com>
2023-08-22 15:48:19 +0200

    fix: remove loading colors

--- src/app/app/search/country/[countryCode]/loading.tsx
+++ src/app/app/search/country/[countryCode]/loading.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Loading() {
   return (
     <div>
-      <div className=' h-10 bg-red-600'></div>
-      <div className='h-6 bg-green-500'></div>
+      <div className=' h-10 '></div>
+      <div className='h-6'></div>
       <div className='flex'>
-        <div className='h-40 w-28 bg-blue-600'></div>
+        <div className='h-40 w-28'></div>
       </div>
     </div>
   );
