f006b00d5bc5a99c7a93995ba42d9c2187b8e3cf
gmwaniki <gmang2236@gmail.com>
2023-08-22 15:25:03 +0200

    feat: navigate to search page on submit

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,19 @@
 'use client';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 import { HiOutlineSearch } from 'react-icons/hi';
 
 export default function Search({ children }: { children?: React.ReactNode }) {
+  const router = useRouter();
+  const [searchValue, setSearchValue] = useState('');
+
+  const onSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    router.push(`/app/search?name=${searchValue}`);
+  };
   return (
     <div className='bg-CustomLightBlack rounded sticky top-1 z-10 sm:bg-CustomLightBlack/80 sm:backdrop-blur-sm sm:top-[10px]'>
-      <form className='flex px-3 pb-3 pt-2'>
+      <form className='flex px-3 pb-3 pt-2' onSubmit={onSubmit}>
         <div className='grid grid-cols-[auto,min-content] auto-rows-auto flex-1'>
           <label
             htmlFor='name'
@@ -19,6 +27,9 @@ export default function Search({ children }: { children?: React.ReactNode }) {
             id='name'
             placeholder='Name'
             className='py-3 pl-2 rounded-l text-lg text-white bg-[#6C6C6C] focus:outline-none focus:ring-2 focus:ring-CustomActive  focus:ring-inset '
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setSearchValue(e.target.value);
+            }}
           />
           <button
             type='submit'
