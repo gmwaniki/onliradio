0070da099c0ce8aa3536bd5b68db9b9bba2f8795
gmwaniki <gmang2236@gmail.com>
2023-08-22 16:01:34 +0200

    chore: migrate metadata

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,11 +1,6 @@
 export default function Head() {
   return (
     <>
-      <title>Onliradio</title>
-      <meta
-        name='description'
-        content='Listen to over 30,000 radio station from over 200 countries '
-      />
       <link rel='icon' href='icon.png' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
     </>
