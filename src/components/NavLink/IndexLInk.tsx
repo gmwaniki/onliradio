7731a7f8af7eee20d1fe99e928244e81eb75c8f9
gmwaniki <gmang2236@gmail.com>
2023-09-02 18:04:59 +0200

    fix:use ouline icons

--- src/components/NavLink/IndexLInk.tsx
+++ src/components/NavLink/IndexLInk.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HiHome, HiOutlineHome } from 'react-icons/hi';
+import { HiOutlineHome } from 'react-icons/hi';
 import NavLink from './NavLink';
 
 export default function IndexLInk() {
@@ -10,8 +10,7 @@ export default function IndexLInk() {
         className='flex flex-col items-center pb-1 pt-2 rounded data-[active=true]:text-white sm:flex-row sm:mx-3 sm:px-4 sm:data-[active=true]:bg-[#151515] sm:gap-2 sm:items-center'
       >
         <div className='sm:group-data-[active=true]:bg-CustomActive sm:text-CustomWhite sm:rounded sm:p-1'>
-          <HiOutlineHome className='text-3xl w-8 h-8 childPath:stroke-1 group-data-[active=true]:hidden' />
-          <HiHome className='text-3xl w-8 h-8 group-data-[active=false]:hidden' />
+          <HiOutlineHome className='text-3xl w-8 h-8 childPath:stroke-1 group-data-[active=true]:childPath:stroke-2' />
         </div>
 
         <span className='group-data-[active=true]:bg-CustomBlack rounded px-4 mx-auto text-center sm:group-data-[active=true]:bg-transparent sm:group-data-[active=true]:text-CustomActive sm:px-0 sm:mx-0'>
