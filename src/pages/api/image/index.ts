0b3d7fe0ebb5c0ae2b97016ac6fba49e62a679eb
gmwaniki <gmang2236@gmail.com>
2023-10-10 22:16:34 +0200

    use webp format

--- src/pages/api/image/index.ts
+++ src/pages/api/image/index.ts
@@ -21,16 +21,17 @@ export default async function handler(
     const responseImageBuffer = await response.arrayBuffer();
     const sharpPipe = sharp(responseImageBuffer);
     const newImage = await sharpPipe
-      .resize(100, 100, {
+      .resize({
         fit: 'contain',
-        width: 100,
-        height: 100,
-        background: { r: 0, g: 0, b: 0, alpha: 0 },
+        width: 250,
+        height: 250,
+        background: { r: 255, g: 255, b: 255, alpha: 0 },
       })
-      .png({ quality: 5, compressionLevel: 9 })
+      .webp({ quality: 20 })
+
       .toBuffer();
 
-    res.setHeader('Content-Type', 'image/png');
+    res.setHeader('Content-Type', 'image/webp');
     res.send(newImage);
   } catch (error) {
     const defaultImage = fs.createReadStream(svgFile);
