62684727875bebabf8a6d6e58eb8db9b82f38161
gmwaniki <gmang2236@gmail.com>
2023-10-17 01:32:33 +0200

    add check to use _ to mark unused variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,15 @@ module.exports = {
   plugins: ['unused-imports', 'simple-import-sort', '@typescript-eslint'],
   rules: {
     'simple-import-sort/imports': 'error',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
     'unused-imports/no-unused-imports': 'error',
   },
   root: true,
