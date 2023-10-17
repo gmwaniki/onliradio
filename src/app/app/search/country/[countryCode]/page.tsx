b1df8a45698da21a5693b14deb4b452d0c1ddf9f
gmwaniki <gmang2236@gmail.com>
2023-10-18 01:03:28 +0200

    fix styling

--- src/app/app/search/country/[countryCode]/page.tsx
+++ src/app/app/search/country/[countryCode]/page.tsx
@@ -28,7 +28,7 @@ export default async function Page({
 
   return (
     <section className='text-CustomWhite'>
-      <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)] items-center justify-center gap-y-5 gap-x-3 py-5'>
+      <ul className='grid grid-flow-row grid-cols-[repeat(auto-fit,150px)] items-center justify-center gap-y-5 gap-x-3 py-5 lg:grid-cols-[repeat(3,minmax(275px,1fr))]'>
         {stations.map((station) => {
           return (
             <li key={station.stationuuid}>
