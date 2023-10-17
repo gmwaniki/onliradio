58133355774d72ff081b9fe1449c7357287891eb
gmwaniki <gmang2236@gmail.com>
2023-10-17 02:00:12 +0200

    access localstorage on page load

--- src/app/providers/LikesContextProvider.tsx
+++ src/app/providers/LikesContextProvider.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 type TContextValues = [
   Record<string, string>,
@@ -9,11 +9,15 @@ type TContextValues = [
 export const LikesContext = createContext<TContextValues>([{}, () => {}]);
 
 const LikesContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [likes, setLikes] = useState<Record<string, string>>(() => {
+  const [likes, setLikes] = useState<Record<string, string>>({});
+  useEffect(() => {
     const likes = localStorage.getItem('likes');
-    if (!likes) return {};
-    return JSON.parse(likes) as Record<string, string>;
-  });
+    if (!likes) {
+      setLikes({});
+    } else {
+      setLikes(JSON.parse(likes));
+    }
+  }, []);
   const handleSetLikes = (newLikes: Record<string, string>) => {
     localStorage.setItem('likes', JSON.stringify(newLikes));
     setLikes(newLikes);
