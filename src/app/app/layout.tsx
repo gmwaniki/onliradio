72e5f8c3632e7ed0dacceee8da4741a691811db7
gmwaniki <gmang2236@gmail.com>
2023-10-22 22:36:27 +0200

    make player logo edges rounded

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -21,12 +21,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       </div>
 
       <nav
-        className=' fixed bottom-0 w-full bg-CustomLightBlack  text-CustomWhite border border-transparent border-t-2 border-t-CustomLightBlack sm:left-0 sm:top-0 sm:bottom-auto  sm:sticky
+        className=' fixed bottom-0 w-full bg-CustomLightBlack  text-CustomWhite sm:left-0 sm:top-0 sm:bottom-auto  sm:sticky 
         sm:max-h-screen sm:col-start-1 sm:row-start-1 sm:pt-[10px] overflow-y-auto'
       >
         <ul className='flex sm:flex-col sm:gap-4 sm:rounded-tr sm:relative sm:z-20 sm:text-xl'>
-          <li className='hidden sm:block mx-3 relative isolate -z-10'>
-            <div className='  bg-CustomBlack py-7 px-1  rounded-l relative -z-10 rounded'>
+          <li className='hidden sm:block isolate -z-10 w-[calc(100%-0.5rem)] ml-auto'>
+            <div className='bg-CustomBlack py-7 px-1  rounded-xl relative -z-10 after:absolute after:-top-12 after:right-0 after:w-5 after:h-12 after:rounded-[0_0_25px_0] after:-z-[1] after:shadow-[0_25px_0_0_#111618] before:absolute before:-top-auto before:right-0 before:w-5 before:h-12 before:rounded-[0_25px_0_0] before:-z-[1] before:-bottom-12 before:shadow-[0_-25px_0_0_#111618]'>
               <Image src={logo} alt='onliradio logo' />
             </div>
           </li>
