c7ee85239302a91b3371d6b3393d191d59f73a4f
gmwaniki <gmang2236@gmail.com>
2023-10-29 18:38:04 +0100

    create station page to redirect

---
+++ src/components/Station/StationPage.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { TStation } from '../../util/playableStation';
+
+const StationPage = ({ station }: { station: TStation }) => {
+  useEffect(() => {
+    const hostUrl = new URL(window.location.href);
+    const stationurl = `${
+      process.env.NODE_ENV === 'production'
+        ? `https://${hostUrl.host}`
+        : `http://localhost:3000`
+    }/app/search?name=${encodeURIComponent(station.name)}&language=${
+      station.language.split(',')[0]
+    }&country=${station.country}`;
+
+    redirect(stationurl);
+  }, [station]);
+  return <></>;
+};
+
+export default StationPage;
