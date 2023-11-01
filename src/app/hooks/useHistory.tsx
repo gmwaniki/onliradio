29256e5c80cdbe43bc432d3ea7c61d39fdf8d32e
gmwaniki <gmang2236@gmail.com>
2023-11-01 22:23:45 +0100

    custom hook to set history and read history

--- src/app/hooks/useHistory.tsx
+++ src/app/hooks/useHistory.tsx
@@ -1,5 +1,34 @@
+import { useContext, useEffect, useState } from 'react';
+
+import { AudioContext } from '../providers/AudioContext';
+
 const useHistory = () => {
-  return {};
+  const [historyStation, setHistoryStations] = useState<Record<string, string>>(
+    {}
+  );
+  const { state } = useContext(AudioContext);
+
+  useEffect(() => {
+    const historyStations = localStorage.getItem('history') as string | null;
+    if (!historyStations) {
+      setHistoryStations({});
+    } else {
+      setHistoryStations(JSON.parse(historyStations) as Record<string, string>);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (state.station.stationId) {
+      setHistoryStations((prev) => {
+        return { ...prev, [state.station.stationId]: '1' };
+      });
+    }
+  }, [state]);
+  useEffect(() => {
+    localStorage.setItem('history', JSON.stringify(historyStation));
+  });
+
+  return { historyStation };
 };
 
 export default useHistory;
