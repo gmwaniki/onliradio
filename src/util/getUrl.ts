61b0cd05cb2e12b7bdc6165689192e3c033abde7
gmwaniki <gmang2236@gmail.com>
2023-12-28 12:01:36 +0100

    use active servers

--- src/util/getUrl.ts
+++ src/util/getUrl.ts
@@ -1,6 +1,24 @@
+import { SrvRecord } from 'node:dns';
+import https from 'node:https';
+
 import { resolveSrv } from 'dns/promises';
 
 export const getUrl = async () => {
   const results = await resolveSrv('_api._tcp.radio-browser.info');
-  return `https://${results[0].name}/json`;
+  const server = await readyServers(results);
+
+  return server;
+};
+
+const readyServers = (servers: SrvRecord[]): Promise<string> => {
+  return new Promise((resolve) => {
+    for (const server of servers) {
+      const serverName = `https://${server.name}/json`;
+      https.get(`${serverName}/stats`, (res) => {
+        if (res.statusCode === 200) {
+          resolve(serverName);
+        }
+      });
+    }
+  });
 };
